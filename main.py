4d3ead990d189b99b4f62f0fd7e50b9591265d3f
uchuuchy <chuchy123987@gmail.com>
2023-11-20 21:47:47 -0500

    Generates a graph of a sine wave

---
+++ main.py
@@ -0,0 +1,15 @@
+# Importing required library
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Creating x axis with range and y axis 
+# Function for Plotting Sine and Cosine Graph
+x = np.arange(0, 5 * np.pi, 0.1)
+y1 = np.sin(x)
+
+# Plotting Sine Graph
+plt.plot(x, y1, color='green')
+plt.savefig("sine.png")
+
+# Source Provided By
+# https://www.geeksforgeeks.org/plotting-sine-and-cosine-graph-using-matloplib-in-python/
